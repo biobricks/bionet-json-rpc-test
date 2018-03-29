69265fa7d6c72e5038f937820252140d2d5443d5
Marc Juul <github@juul.io>
2018-03-29 04:44:55 -0700

    authentication working

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from jsonrpc_requests import Server
+from jsonrpc_requests import Server, ProtocolError
 
 
 def manual():
@@ -27,14 +27,37 @@ def main():
 
     bionet = Server('http://localhost:3000/rpc')
 
-    res = bionet.foo('lol')
-    print res
+    res = bionet.foo('foo')
+    print "no stream: %s" % res
 
-    res = bionet.bar('sfa')
-    print res
+    res = bionet.bar()
+    print "return stream: %s" % res
 
-    res = bionet.baz('sfa')
-    print res
+    res = bionet.baz('a')
+    print "callback stream: %s" % res
+
+    try:
+        res = bionet.fail()
+    except ProtocolError as err:
+        print("got error: %s" % err.message)
+        print("just the error message: %s" % err.server_data['error']['message'])
+
+    try:
+        res = bionet.secret()
+    except ProtocolError as err:
+        print("while calling .secret(): %s" % err.message)
+
+    res = bionet.login('foo', 'bar')
+    # note: your probably won't ever need to use this token manually
+    print "logged in and got token: %s" % res 
+
+    res = bionet.secret()
+    print "secret: %s" % res
+
+    try:
+        res = bionet.admin_secret()
+    except ProtocolError as err:
+        print("while calling .admin_secret(): %s" % err.message)
 
 if __name__ == "__main__":
     main()
--- server.js
+++ server.js
@@ -7,22 +7,88 @@ var http = require('http');
 var router = require('routes')(); // server side router
 var jayson = require('jayson');
 var rpc = require('rpc-multistream');
+var auth = require('rpc-multiauth');
 
-var jsonRPC = jayson.server(unstreamify({
+var myAuth = auth({
+  secret: "secret",
+  cookie: {
+    setCookie: true
+  }
+});
 
-  foo: function(f, cb) {
-    cb(null, f + ": bar");
+function login(res, username, password, cb) {
+
+  if(username !== 'foo' || password !== 'bar') {
+    return cb(new Error("invalid username or password"));
+  }
+
+  var userData = {
+    username: username, 
+    password: password,
+    group: 'user'
+  };
+
+  myAuth.login(res, userData.username, userData, function(err, token) {
+    // a bug in old versions of rpc-multiauth
+    // sometimes means we get a string instead of an error object back
+    if(typeof err === 'string') err = new Error(err);
+    if(err) return cb(err);
+
+    cb(null, token);
+  });
+}
+var foo;
+
+var jsonRPC = jayson.server(foo = unstreamify(login, {
+
+  foo: function(curUser, f, cb) {
+    console.log("ARGS:", arguments);
+    cb(null, f + ": bar", null, 3);
   },
 
-  bar: rpc.syncReadStream(function(a) {
-    return fs.createReadStream('./server.js', {encoding: 'utf8'});
+  bar: rpc.syncReadStream(function(curUser, a) {
+    return fs.createReadStream('./test.txt', {encoding: 'utf8'});
   }),
 
-  baz: function(a, cb) {
-    cb(null, 'lol', fs.createReadStream('./server.js', {encoding: 'utf8'}));
+  baz: function(curUser, a, cb) {
+    cb(null, a+'lol', fs.createReadStream('./test.txt', {encoding: 'utf8'}));
+  },
+
+  fail: function(curUser, cb) {
+    cb(new Error("something bad happened"));
+  },
+
+  user: {
+    secret: function(curUser, cb) {
+      cb(null, 42);
+    }
+  },
+
+  admin: {
+    admin_secret: function(curUser, cb) {
+      cb(null, "praise bob");
+    }
   }
 
 })).middleware();
+
+console.log(Object.keys(foo));
+
+// inject a new first argument into a JSON-RPC call object
+function injectFirstArg(obj, arg) {
+  if(!obj.params) {
+    obj.params = [arg];
+  } else {
+    obj.params = [arg].concat(obj.params);
+  }
+  console.log("OBJECT:", obj);
+  return obj;
+}
+
+// convert normal js error to JSON-RPC error object
+function jsonError(err, code) {
+  return {code: code || 500, message: err.message || "Unknown error"};
+}
 
 function streamToArray(s, cb) {
   var onlyStrings = true;
@@ -66,21 +132,21 @@ function unstreamifyResult(res, cb) {
   }
 }
 
-function unstreamifyFunction(f) {
-  return function() {
+function unstreamifyFunction(f, group) {
+  return function(args, cb) {
     var res;
 
-    // assume function is async if last argument is a callback
-    if(!arguments.length || typeof arguments[arguments.length-1] !== 'function') {
-      // we don't support functions without a callback
-      // since JSON-RPC 2.0 does not support streams
-      // and stream-returning functions are the only non-async functions allowed
-      return;
+    if(group) {
+      var userData = args[0];
+
+      console.log("USER DATA:", userData);
+      if(!userData) {
+        return cb(jsonError(new Error("You must be logged in to access this function"), 401));
+      }
+      if(userData.group !== group) {
+        return cb(jsonError(new Error("You must be in the " + group + " group to access this function"), 401));
+      }
     }
-
-    var cb = arguments[arguments.length-1];
-
-    var args = Array.prototype.slice.call(arguments, 0, arguments.length-1);
 
     if(f._rpcOpts) { // is this an rpc-multistream syncStream function?
       if(f._rpcOpts.type === 'w') {
@@ -88,35 +154,58 @@ function unstreamifyFunction(f) {
       }
 
       res = f.apply(null, args);
-      unstreamifyResult(res, cb)
+      unstreamifyResult(res, function(err, cbArgs) {
+        if(err) return cb(jsonError(err));
+
+        cb(null, [cbArgs]);
+      })
 
     } else {
 
       args[args.length] = function() {
         if(arguments.length && arguments[0]) {
-          return cb(arguments[0]);
+          return cb(jsonError(arguments[0]));
         }
 
-        var cbArgs = Array.prototype.slice.call(arguments, 0); // convert to array
+        var cbArgs = Array.prototype.slice.call(arguments, 1); // convert to array
+
         
-        unstreamifyResult(cbArgs, cb);
+        unstreamifyResult(cbArgs, function(err, cbArgs) {
+          if(err) return cb(jsonError(err));
+
+          cb(null, cbArgs);
+        });
+
       };
       f.apply(null, args);
     }
   }
 }
 
-function unstreamify(methods) {
-  if(typeof methods === 'function') {
-    return unstreamifyFunction(methods) 
-  }
 
-  var o = {};
+function unstreamify(loginMethod, methods, o, group) {
+
+  o = o || {};
 
   var fname;
   for(fname in methods) {
-    o[fname] = unstreamifyFunction(methods[fname]);
+    if(!group && typeof methods[fname] === 'object') {
+      unstreamify(loginMethod, methods[fname], o, fname);
+      continue;
+    }
+
+    o[fname] = unstreamifyFunction(methods[fname], group);
   }
+
+  if(loginMethod) {
+    o.login = function(args, cb) {
+      loginMethod.apply(null, args.concat([function(err) {
+        if(err) return cb(jsonError(err));
+
+        cb.apply(null, arguments);
+      }]));
+    }
+  };
 
   return o;
 }
@@ -151,16 +240,31 @@ router.addRoute('/rpc', function(req, res, match) {
       res.end("Bad request: Invalid JSON");
       return;
     }    
-    
-    req.body = data;
-    jsonRPC(req, res);
 
+    // TODO we shouldn't do this if not using auth at all
+    if(data.method === 'login') {
+      data = injectFirstArg(data, res);
+      req.body = data;
+      jsonRPC(req, res);
+      return;
+    }
+
+    myAuth(req, function(err, tokenData) {
+      if(err) {
+        data = injectFirstArg(data, null);
+      } else {
+        data = injectFirstArg(data, tokenData);
+      }
+
+      req.body = data;
+      jsonRPC(req, res);
+
+    });
   });
 });
 
 router.addRoute('/*', function(req, res, match) {
   res.end("<html><body>Main page</body></html>");
 });
-
 
 server.listen(3000, 'localhost');
